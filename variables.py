02e08fec303c45a68204366e9d3dbef97094a164
BiFr0st <11083104-BiFr0st@users.noreply.replit.com>
2023-10-26 21:53:42 +0000

    Quit messages

--- variables.py
+++ variables.py
@@ -13,4 +13,4 @@ title = [
 audio_titles = ["title","background","bar","rain","finale"]
 help_text = ["go <inside,forward,left,right,up,down>","get <x>", "yes","no","back","inventory","If there is no clear path forward, look around..."] #Commands in the game
 death_text = ["PROTOCOL <3","L+Bozo+Ratio+Fell off+Dead","Damn, you dead shawty.","You died, you dead.","That grue absolutely bodyslammed you.","I knew I should've got the shiny one.","At least you get to see Jackie again.","Nobody really cares.","Just another statistic.","Crap, did I turn off the stove?","Did you hug your dog before you left?","Are you happy now?"]
-quit_text = [""]
+quit_text = ["What did I do wrong...","Only 10k hours spent in this game? Pathetic","Come back later..."]
